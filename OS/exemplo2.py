c1bf2949357f04fd8137893d97f312a0306e5073
Beatriz Vasconcelos <81173172+beatrixvascc@users.noreply.github.com>
2023-01-30 00:52:40 -0300

    Add files via upload

---
+++ OS/exemplo2.py
@@ -0,0 +1,5 @@
+import os
+if os.path.exists(".vscode\OS\pasta 2"):
+    print("ja existe")
+else:    
+    os.mkdir(".vscode/OS/pasta 2")
---
+++ OS/remover.py
@@ -0,0 +1,2 @@
+import os
+os.rmdir("C:\\Users\\beatr\\OneDrive\\Desktop\\python project 1\\.vscode\\OS\\exemplo")
